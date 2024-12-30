c9ce3cd68ccd3a29fd89be1661e153e98e29218a
elvinofae <149983380+elvinofae@users.noreply.github.com>
2024-12-30 09:34:04 -0300

    feature: commit inicial
    - Inclusao do commit inicial com a arquitetura template

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+
+[ExcludeFromCodeCoverage]
+public class DomainException:Exception
+{
+    public DomainException(string message):base(message)
+    {
+    }
+
+    public DomainException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
